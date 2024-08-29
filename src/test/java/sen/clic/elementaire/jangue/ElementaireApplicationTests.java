6aeafa757935ada0e05ffc7829d3137fd78b0c53
Moz <MoiMemeMoussa@users.noreply.github.com>
2024-08-29 14:27:06 -0400

    First commit
    First commit: structure du projet

---
+++ src/test/java/sen/clic/elementaire/jangue/ElementaireApplicationTests.java
@@ -0,0 +1,13 @@
+package sen.clic.elementaire.jangue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = ElementaireApplicationTests.class)
+class ElementaireApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
