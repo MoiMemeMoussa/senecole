7aecdabd3804c4379d7f32f839d1ad035feff821
Moz <MoiMemeMoussa@users.noreply.github.com>
2024-08-29 14:27:06 -0400

    First commit
    First commit: structure du projet

---
+++ src/main/java/sen/ecole/primaire/ElementaireApplication.java
@@ -0,0 +1,13 @@
+package sen.ecole.primaire;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElementaireApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ElementaireApplication.class, args);
+    }
+
+}
